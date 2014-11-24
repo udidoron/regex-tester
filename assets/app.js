33da524607df65100c2956bcac26eab5a0487e80
udidoron <ehud.doron@mail.huji.ac.il>
2014-11-24 16:39:16 +0200

    Code manages to recognize all appearances of regex in text field and retrieve them.

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,103 @@
 $(document).ready(function() {
-	var clickedText = false;
-	var startingTextSpan = "<span id='startingText'>Enter your text here. </span>";
-	if (!clickedText) {
-		$("#text").html(startingTextSpan);
+
+	//"Regex" and "Text" field design and initialization code
+	function initializeArea(id, spanCode) {
+		var clickedArea = false;
+		var jqId = "#"+id; //I hate reusing quote marks and plus signs
+		$(jqId).html(spanCode);
+
+		$(jqId).click(function() {
+			if (!clickedArea) {
+				$(jqId).html("");
+				clickedArea=true;
+			}
+		});
+
+		$(jqId).keypress(function() {
+			if ($(this).children(".startingText").size()>0) {
+				$(this).html("");				
+			}
+		});
+
+		$(document).click(function(e) {
+			if (e.target.id !== id && $.trim($(jqId).text()) == "") {
+				$(jqId).html(spanCode);
+				clickedArea=false;
+			}
+		});
 	}
-	$("#text").click(function(e) {
-		if (!clickedText) {
-			$("#text").html("");
-			clickedText=true;
+
+	var startingRegexSpan = "<span class='startingText'> Enter your regex here. </span>";
+	var startingTextSpan = "<span class='startingText'>Enter your text here. </span>";
+	initializeArea("regex", startingRegexSpan);
+	initializeArea("text", startingTextSpan);
+
+
+
+	//Regex initialization and loading code
+	var testedRegex;
+	var modifiers = "g";
+	var regexChanged = false;
+	var modifiersChanged = false;
+
+	//Tiny bit of optimization code (yeah yeah, root of all evil, I know)
+	var iCheckboxJqId = "#options_case_insensitive_checkbox";
+	$(iCheckboxJqId).prop("checked", true);
+	var mCheckboxJqId = "#options_search_multilines_checkbox";
+	$(mCheckboxJqId).prop("checked", false);
+
+	$(iCheckboxJqId, mCheckboxJqId).click(function(){ 
+		modifiersChanged=true; 
+		console.log("modifier changed");
+	});
+
+	$("#regex").blur(function() {
+		regexChanged=true;
+		if ($.trim($(this).text()) !== "") {
+			testedRegex=true; //it's kinda ugly, I know
 		}
 	});
-	$(document).click(function(e) {
-		if (e.target.id != "text" && $("#text").text().trim() == "") {
-			$("#text").html(startingTextSpan);
-			clickedText=false;
-		}
-	})
-})
 
-function initiate() {
-	document.getElementById("text").addEventListener("keyup", function(e) {
-		var elem = e.target;
-		var reg = /haw haw/gi;
-		console.log(reg.exec(elem.innerHTML));
-	});	
-}
+	function checkRegex(id) {
+		if (!testedRegex && id !== "regex") return; //no regex yet
+
+		if (regexChanged || modifiersChanged) {
+			//checking for /i, /m
+			modifiers="g";
+			if ($(iCheckboxJqId).prop("checked")) {modifiers += "i"};
+			if ($(mCheckboxJqId).prop("checked")) {modifiers += "m"};
+
+			//retrieving regex pattern and creating regex
+			var pattern = $("#regex").text();
+			if ($.trim(pattern)!=="") {
+				testedRegex = new RegExp(pattern, modifiers);
+				regexChanged = modifiersChanged = false;
+			}
+		}
+
+		if (!regexChanged) {
+			var jqId = "#"+id;
+			var exec = testedRegex.exec($("#text").text());
+			var arr = [];
+			while(exec) {
+				arr.push(exec);
+				exec = testedRegex.exec($("#text").text());
+			}
+			if (arr.length>0) {
+				// colorTextBackground(arr);
+				console.log(arr);
+			}
+		}
+	}
+
+	$("#regex").keyup(function() {
+		regexChanged=true;
+		checkRegex("regex");
+	});
+
+	$("#text").keyup(function() {
+		checkRegex("text");
+	});
+
+});
+
